2d399859de459936d6b851d5decca48ae135ef18
David Ung <david@ung.io>
2017-03-24 14:32:58 -0700

    feat(pages): Adds Material UI

--- gh-pages/src/App.js
+++ gh-pages/src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AutoComplete from 'material-ui/AutoComplete';
+
 import logo from './logo.svg';
 import './App.css';
+
+injectTapEventPlugin();
+
+const colors = [
+  'Red',
+  'Orange',
+  'Yellow',
+  'Green',
+  'Blue',
+  'Purple',
+  'Black',
+  'White',
+  'Edward',
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <AutoComplete
+            floatingLabelText="Type 'r', case insensitive"
+            filter={AutoComplete.caseInsensitiveFilter}
+            dataSource={colors}
+          />
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
 
 export default App;
+
